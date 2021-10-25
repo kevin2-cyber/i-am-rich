e59816c8da748d0605db5bb03098aff8c8981161
kevin2-cyber <73193206+kevin2-cyber@users.noreply.github.com>
2021-10-24 19:12:13 -0700

    added a networked image  to flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+// The main function is the starting point of all our flutter apps.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://c8.alamy.com/comp/2BHG705/colourful-conceptual-images-2BHG705.jpg'),
+          ),
+        ),
       ),
-    ));
+      ),
+  );
+}
